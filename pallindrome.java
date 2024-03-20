68a47b4312b798eb73d63c8272f92040cd39efd8
Kowshika29-K <161927842+Kowshika29-K@users.noreply.github.com>
2024-03-20 18:36:09 +0530

    Add files via upload

---
+++ pallindrome.java
@@ -0,0 +1,44 @@
+public class pallindrome {
+    public static void main(String[] args) {
+        String[] words = {"abc","bcd","aba","cfg","madam"};
+
+        String palindrome = findFirstPalindrome(words);
+        if (palindrome != null) {
+            System.out.println("First palindrome word: " + palindrome);
+        } else {
+            System.out.println("No palindrome word found.");
+        }
+    }
+
+    public static String findFirstPalindrome(String[] words) {
+        for (String word : words) {
+            if (isPalindrome(word)) {
+                return word;
+            }
+        }
+        return null;
+    }
+
+    public static boolean isPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+              left++;
+             right--;
+        }
+        return true;
+    }
+}
+
+
+
+// string s="madam",rs="";
+
+// for(i=s.length()-1;i>=0;i--)
+// rs=rs+s.charAt(i);
+
+// s==rs
---
+++ word.java
@@ -0,0 +1,19 @@
+public class word {
+    public static void main(String[] args) {
+        String[] words = {"abc", "bcd", "aaaa", "cbc"};
+        char x = 'a';
+
+        System.out.print("Indices of words containing '" + x + "': [");
+        boolean found = false;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].indexOf(x) != -1) {
+                if (found) {
+                    System.out.print(", ");
+                }
+                System.out.print(i);
+                found = true;
+            }
+        }
+        System.out.println("]");
+    }
+}
